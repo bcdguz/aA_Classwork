9933f41efc6d3c687e4ce74bd7dcebcf428b4a92
Edwin Cheng <edwincheng3@gmail.com>
2020-12-04 15:12:07 -0800

    Knights v2 - 15 min

--- w3d5/knights_travails.rb
+++ w3d5/knights_travails.rb
@@ -64,15 +64,38 @@ class KnightPathFinder
 
     def self.valid_moves(pos)
         possible_moves = []
-        possible_moves << [pos[0]+1, pos[1]+2] if !@considered_positions.include?([pos[0]+1, pos[1]+2])
-        possible_moves << [pos[0]-1, pos[1]+2]
-        possible_moves << [pos[0]+1, pos[1]-2]
-        possible_moves << [pos[0]-1, pos[1]-2]
-        possible_moves << [pos[0]+2, pos[1]+1]
-        possible_moves << [pos[0]-2, pos[1]+1]
-        possible_moves << [pos[0]+2, pos[1]-1]
-        possible_moves << [pos[0]-2, pos[1]-1]
+
+        move_1 = [pos[0]+1, pos[1]+2]
+        possible_moves << move_1 if in_bounds?(move_1)
+
+        move_2 = [pos[0]-1, pos[1]+2] 
+        possible_moves << move_2 if in_bounds?(move_2)
+
+        move_3 = [pos[0]+1, pos[1]-2]
+        possible_moves << move_3 if in_bounds?(move_3)
+
+        move_4 = [pos[0]-1, pos[1]-2]
+        possible_moves << move_4 if in_bounds?(move_4)
+
+        move_5 = [pos[0]+2, pos[1]+1]
+        possible_moves << move_5 if in_bounds?(move_5)
+
+        move_6 = [pos[0]-2, pos[1]+1] 
+        possible_moves << move_6 if in_bounds?(move_6)
+
+        move_7 = [pos[0]+2, pos[1]-1]
+        possible_moves << move_7 if in_bounds?(move_7)
+
+        move_8 = [pos[0]-2, pos[1]-1]
+        possible_moves << move_8 if in_bounds?(move_8)
+
         possible_moves
+    end
+
+    # Helper Method
+    def in_bounds?(pos)
+        return false if pos[0] < 0 || pos[0] > 7
+        return false if pos[1] < 0 || pos[1] > 7
     end
 
     def build_move_tree
@@ -81,6 +104,13 @@ class KnightPathFinder
 
     def new_move_positions(pos)
 
+        filtered_pos = KnightPathFinder.valid_moves(pos)
+        answers = filtered_pos.select do |position|
+            !@considered_positions.include?(position)
+        end
+
+        @considered_positions += answers
+        answers
     end
 
 end

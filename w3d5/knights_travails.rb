d93456bbf63c37956a4567752ba712a1e75d1da1
bcdguz <bcdguz96@gmail.com>
2020-12-04 16:06:32 -0800

    Changing driver/nav for knights_travails

--- w3d5/knights_travails.rb
+++ w3d5/knights_travails.rb
@@ -56,6 +56,7 @@ end
 
 
 class KnightPathFinder
+    attr_reader :root_node, :considered_positions
 
     def initialize(starting_pos)
         @root_node = PolyTreeNode.new(starting_pos)
@@ -66,40 +67,45 @@ class KnightPathFinder
         possible_moves = []
 
         move_1 = [pos[0]+1, pos[1]+2]
-        possible_moves << move_1 if in_bounds?(move_1)
+        possible_moves << move_1 if KnightPathFinder.in_bounds?(move_1)
 
         move_2 = [pos[0]-1, pos[1]+2] 
-        possible_moves << move_2 if in_bounds?(move_2)
+        possible_moves << move_2 if KnightPathFinder.in_bounds?(move_2)
 
         move_3 = [pos[0]+1, pos[1]-2]
-        possible_moves << move_3 if in_bounds?(move_3)
+        possible_moves << move_3 if KnightPathFinder.in_bounds?(move_3)
 
         move_4 = [pos[0]-1, pos[1]-2]
-        possible_moves << move_4 if in_bounds?(move_4)
+        possible_moves << move_4 if KnightPathFinder.in_bounds?(move_4)
 
         move_5 = [pos[0]+2, pos[1]+1]
-        possible_moves << move_5 if in_bounds?(move_5)
+        possible_moves << move_5 if KnightPathFinder.in_bounds?(move_5)
 
         move_6 = [pos[0]-2, pos[1]+1] 
-        possible_moves << move_6 if in_bounds?(move_6)
+        possible_moves << move_6 if KnightPathFinder.in_bounds?(move_6)
 
         move_7 = [pos[0]+2, pos[1]-1]
-        possible_moves << move_7 if in_bounds?(move_7)
+        possible_moves << move_7 if KnightPathFinder.in_bounds?(move_7)
 
         move_8 = [pos[0]-2, pos[1]-1]
-        possible_moves << move_8 if in_bounds?(move_8)
+        possible_moves << move_8 if KnightPathFinder.in_bounds?(move_8)
 
         possible_moves
     end
 
     # Helper Method
-    def in_bounds?(pos)
+    def self.in_bounds?(pos)
         return false if pos[0] < 0 || pos[0] > 7
         return false if pos[1] < 0 || pos[1] > 7
+        return true
     end
 
     def build_move_tree
-
+        queue = new_move_positions(@root_node.value)
+        queue.each do |position|
+            child = PolyTreeNode.new(position)
+            @root_node.add_child(child)
+        end
     end
 
     def new_move_positions(pos)

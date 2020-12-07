9a511270366b3435918bbf3ccdcb8229970d068a
bcdguz <bcdguz96@gmail.com>
2020-12-07 13:53:30 -0800

    Started tic-tac-toe; 15 min switch

---
+++ w4d1/Tic-Tac-Toe AI/lib/tic_tac_toe_node.rb
@@ -0,0 +1,44 @@
+require_relative 'tic_tac_toe'
+require 'byebug'
+class TicTacToeNode
+  attr_reader :board, :next_mover_mark, :prev_move_pos
+
+  def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
+  end
+
+  def losing_node?(evaluator)
+  end
+
+  def winning_node?(evaluator)
+  end
+
+  # This method generates an array of all moves that can be made after
+  # the current move.
+  def children
+    possible_pos = []
+    @board.rows.each_with_index do |el1, i1|
+      @board.rows.each_with_index do |el2, i2|
+        possible_pos << [i1, i2] if @board.empty?([i1, i2])
+      end
+    end
+    children_arr = []
+    child_mark = nil
+    if @next_mover_mark == :x
+      child_mark = :o
+    else  
+      child_mark = :x
+    end
+    possible_pos.each do |pos|
+      
+      dup_board = @board.dup
+      dup_board[pos] = @next_mover_mark
+      child_node = TicTacToeNode.new(dup_board, child_mark, pos)
+
+      children_arr << child_node
+    end
+    children_arr
+  end
+end

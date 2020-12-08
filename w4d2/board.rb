95b925bef45844fd5b9b53096e991e1e93294916
droid4alex <droid4alex@gmail.com>
2020-12-08 15:27:32 -0800

    Worked on Chess phase 1 for 15m

---
+++ w4d2/board.rb
@@ -0,0 +1,33 @@
+class Board
+  def initialize
+    @rows = Array.new(8){Array.new(8)}
+    @null_piece = nil             #come back to this... singleton
+  end
+
+  def test_print
+    @rows.each do |row|
+      p row
+    end
+    return nil
+  end
+
+  def [](pos)
+    @rows[pos[0]][pos[1]]
+  end
+
+  def []=(pos, value)
+    @rows[pos[0]][pos[1]] = value
+  end
+
+
+  def fill_board
+    @rows.each_with_index do |row, i|
+      if i == 1
+    end
+  end
+
+
+end
+
+b = Board.new
+b.test_print

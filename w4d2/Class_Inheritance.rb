76daa582ddc47a17f0a89af11922fb81d0850b90
bcdguz <bcdguz96@gmail.com>
2020-12-08 14:55:24 -0800

    Completed Class_Inhereitance.rb

--- w4d2/Class_Inheritance.rb
+++ w4d2/Class_Inheritance.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 class Employee
     attr_reader :salary
 
@@ -6,7 +7,7 @@ class Employee
         @title = title
         @salary = salary
         @boss = boss
-
+        @boss.input_employee(self) if @boss != nil
     end
 
     def bonus(multiplier)
@@ -25,16 +26,28 @@ class Manager < Employee
     end
 
     def input_employee(employee)
+
         @employees << employee
     end
 
     def bonus(multiplier)
+        subordinates = [self]
         super(multiplier)
-        employee_salary = 0
-        @employees.each do |employee|
-            employee_salary += employee.salary
+        employees_salary = 0
+        while !subordinates.empty?
+            current_emp = subordinates.shift
+            if current_emp.is_a?(Manager)
+                current_emp.employees.each do |employee|
+                    subordinates << employee
+                end
+            end
+            #debugger
+            employees_salary += current_emp.salary
         end
-        @bonus = multiplier * employee_salary
+        
+            
+        employees_salary -= self.salary
+        @bonus = multiplier * employees_salary
     end
     
 
@@ -45,10 +58,10 @@ Darren = Manager.new("Darren", "TA Manager", 78000, Ned)
 Shawna = Employee.new("Shawna", "TA", 12000, Darren)
 David = Employee.new("David", "TA", 10000, Darren)
 
-Ned.input_employee(Darren)
-Darren.input_employee(Shawna)
-Darren.input_employee(David)
-p Darren.employees.length
+# Ned.input_employee(Darren)
+# Darren.input_employee(Shawna)
+# Darren.input_employee(David)
+# p Darren.employees.length
 p Ned.bonus(5)
 p Darren.bonus(4)
 p David.bonus(3)

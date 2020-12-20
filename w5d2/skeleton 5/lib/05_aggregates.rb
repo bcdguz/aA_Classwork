dcfaa2b16efd18a68f904fdcfcf56900b4884897
bcdguz <bcdguz96@gmail.com>
2020-12-20 11:51:25 -0800

    Working through files 04-07 in sqlzoo

--- w5d2/skeleton 5/lib/05_aggregates.rb
+++ w5d2/skeleton 5/lib/05_aggregates.rb
@@ -12,13 +12,16 @@ require_relative './sqlzoo.rb'
 
 def example_sum
   execute(<<-SQL)
-
+  SQL
 end
 
 def continents
   # List all the continents - just once each.
   execute(<<-SQL)
-
+    SELECT
+      DISTINCT continent
+    FROM
+      countries;
     
   SQL
 end
@@ -26,13 +29,24 @@ end
 def africa_gdp
   # Give the total GDP of Africa.
   execute(<<-SQL)
-
+    SELECT
+      SUM(gdp) AS total_gdp
+    FROM
+      countries
+    WHERE
+      continent = 'Africa';
   SQL
 end
 
 def area_count
   # How many countries have an area of more than 1,000,000?
   execute(<<-SQL)
+    SELECT
+      COUNT(name) AS area_count
+    FROM
+      countries
+    WHERE
+      area > 1000000;
 
   SQL
 end
@@ -40,13 +54,24 @@ end
 def group_population
   # What is the total population of ('France','Germany','Spain')?
   execute(<<-SQL)
-    
+    SELECT
+      SUM(population) AS group_pop
+    FROM
+      countries
+    WHERE
+      name IN ('France','Germany','Spain');
   SQL
 end
 
 def country_counts
   # For each continent show the continent and number of countries.
-  execute(<--SQL)
+  execute(<<-SQL)
+    SELECT
+      continent, COUNT(name) AS num_countries
+    FROM
+      countries
+    GROUP BY
+      continent;
   SQL
 end
 
@@ -54,13 +79,27 @@ def populous_country_counts
   # For each continent show the continent and number of countries with
   # populations of at least 10 million.
   execute(<<-SQL)
-
+    SELECT
+      continent, COUNT(name) AS country
+    FROM
+      countries
+    WHERE
+      population >= 10000000
+    GROUP BY
+      continent;
   SQL
 end
 
 def populous_continents
   # List the continents that have a total population of at least 100 million.
   execute(<<-SQL)
-\
+    SELECT
+      continent
+    FROM
+      countries
+    GROUP BY
+      continent
+    HAVING
+      SUM(population) >= 100000000;
   SQL
 end

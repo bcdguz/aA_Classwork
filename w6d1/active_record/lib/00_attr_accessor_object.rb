090172881c6a98efaf7b3606dae043ed6852adf7
bcdguz <bcdguz96@gmail.com>
2020-12-28 14:36:33 -0800

    Adding ::my_attr_acessor to active_record

--- w6d1/active_record/lib/00_attr_accessor_object.rb
+++ w6d1/active_record/lib/00_attr_accessor_object.rb
@@ -1,5 +1,17 @@
+
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+
+    names.each do |var|
+      inst_var = '@' + var.to_s
+
+      define_method(var) do 
+        instance_variable_get(inst_var)
+      end
+      
+      define_method("#{var}=") do |val|
+        instance_variable_set(inst_var, val)
+      end
+    end
   end
 end

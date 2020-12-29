bb68bc8a6e03ad32e17ff6bb135bcfe2a8eb9791
Michelle Ha <michelle1992us@yahoo.com>
2020-12-29 15:06:56 -0800

    finsihed up to update

--- w6d3/routes_and_controllers/app/controllers/users_controller.rb
+++ w6d3/routes_and_controllers/app/controllers/users_controller.rb
@@ -6,11 +6,35 @@ class UsersController < ApplicationController
     def create
         user = User.new(params.require(:user).permit(:name,:email))
         # replace the `user_attributes_here` with the actual attribute keys
-        user.save!
-        render json: user
+        if user.save
+            render json: user
+          else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
     def show
-        render json: params
+        user = User.find(params[:id])
+        render json: user
+    end
+
+        def update
+          user = User.find(params[:id])
+      
+          if user.update(user_params)
+            redirect_to user_url(user)
+          else
+            render json: user.errors.full_messages, status: 422
+          end
+        end
+
+    # def delete
+
+    # end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email)
     end
 end

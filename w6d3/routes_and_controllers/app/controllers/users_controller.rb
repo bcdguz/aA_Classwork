963e7e3dc6ef091c549c1ff5d81cea7e468f7284
bcdguz <bcdguz96@gmail.com>
2020-12-29 15:12:02 -0800

    Completed routes_and_controllers rails proj

--- w6d3/routes_and_controllers/app/controllers/users_controller.rb
+++ w6d3/routes_and_controllers/app/controllers/users_controller.rb
@@ -18,19 +18,21 @@ class UsersController < ApplicationController
         render json: user
     end
 
-        def update
-          user = User.find(params[:id])
-      
-          if user.update(user_params)
-            redirect_to user_url(user)
-          else
-            render json: user.errors.full_messages, status: 422
-          end
-        end
+    def update
+      user = User.find(params[:id])
+  
+      if user.update(user_params)
+        redirect_to user_url(user)
+      else
+        render json: user.errors.full_messages, status: 422
+      end
+    end
 
-    # def delete
-
-    # end
+    def destroy
+      user = User.find(params[:id])
+      user.destroy
+      render json: user  
+    end
 
     private
 

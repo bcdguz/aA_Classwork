fb09283a8fb99dea32034554872d175cd65cd22b
Michelle Ha <michelle1992us@yahoo.com>
2020-12-29 15:40:38 -0800

    finished artwork table/model

--- w6d3/routes_and_controllers/app/models/artwork.rb
+++ w6d3/routes_and_controllers/app/models/artwork.rb
@@ -1,2 +1,21 @@
+# == Schema Information
+#
+# Table name: artworks
+#
+#  id         :bigint           not null, primary key
+#  title      :string           not null
+#  img_url    :string           not null
+#  artist_id  :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Artwork < ApplicationRecord
+
+    validates :title, uniqueness: {scope: :artist_id}
+    validates :title, :artist_id, img_url, presence: true
+
+    belongs_to :artist,
+        primary_key: :id,
+        foreign_key: :artist_id,
+        class_name: :User
 end

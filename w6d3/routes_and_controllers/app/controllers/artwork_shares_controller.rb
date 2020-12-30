3e1e5a9210b5756c583f723dc0d25ef67ecfb4f0
Michelle Ha <michelle1992us@yahoo.com>
2020-12-29 18:02:15 -0800

    added params.require

--- w6d3/routes_and_controllers/app/controllers/artwork_shares_controller.rb
+++ w6d3/routes_and_controllers/app/controllers/artwork_shares_controller.rb
@@ -16,5 +16,11 @@ class ArtworkSharesController < ApplicationController
         render json: artworkshare
       end
 
+    private
+
+    def artwork_share_params
+        params.require(:artwork_share).permit(:artwork_id, :viewer_id)
+    end
+
 
 end

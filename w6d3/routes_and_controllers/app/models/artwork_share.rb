752770f5349ed9be0019506f915f3be15fdf081d
Michelle Ha <michelle1992us@yahoo.com>
2020-12-29 16:11:41 -0800

    finsihed artwork_share table

--- w6d3/routes_and_controllers/app/models/artwork_share.rb
+++ w6d3/routes_and_controllers/app/models/artwork_share.rb
@@ -1,2 +1,4 @@
 class ArtworkShare < ApplicationRecord
+    # validates :title, uniqueness: {scope: :artist_id}
+    # validates :title, :artist_id, img_url, presence: true
 end
--- w6d3/routes_and_controllers/db/migrate/20201229233551_create_artwork_shares.rb
+++ w6d3/routes_and_controllers/db/migrate/20201229233551_create_artwork_shares.rb
@@ -5,5 +5,9 @@ class CreateArtworkShares < ActiveRecord::Migration[5.2]
       t.integer :viewer_id, null: false
       t.timestamps
     end
+    add_index :artwork_shares, [:artwork_id, :viewer_id], unique: true
+    add_index :artwork_shares, :artwork_id
+    add_index :artwork_shares, :viewer_id
+
   end
 end

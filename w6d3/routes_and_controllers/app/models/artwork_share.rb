c2a4313bb619f71c1f9cb3f277ba6478ff077361
bcdguz <bcdguz96@gmail.com>
2020-12-29 16:35:08 -0800

    Adding new routes

--- w6d3/routes_and_controllers/app/models/artwork_share.rb
+++ w6d3/routes_and_controllers/app/models/artwork_share.rb
@@ -1,4 +1,24 @@
+# == Schema Information
+#
+# Table name: artwork_shares
+#
+#  id         :bigint           not null, primary key
+#  artwork_id :integer          not null
+#  viewer_id  :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class ArtworkShare < ApplicationRecord
-    # validates :title, uniqueness: {scope: :artist_id}
-    # validates :title, :artist_id, img_url, presence: true
+    validates :artwork_id, uniqueness: {scope: :viewer_id}
+    validates :artwork_id, :viewer_id, presence: true
+
+    belongs_to :artwork,
+        primary_key: :id,
+        foreign_key: :artwork_id,
+        class_name: :Artwork
+    
+    belongs_to :viewer,
+        primary_key: :id,
+        foreign_key: :viewer_id,
+        class_name: :User
 end
--- w6d3/routes_and_controllers/db/seeds.rb
+++ w6d3/routes_and_controllers/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user1 = User.new(username: 'Chippy')
+user2 = User.new(username: 'Potato')
+
+artwork1 = Artwork.new(title: 'Rainbow', artist_id: 1, img_url: 'pretty.com')
+artwork2 = Artwork.new(title: 'Foods', artist_id: 2, img_url: 'fatty.com')
+
+artwork_share1 = ArtworkShare.new(artwork_id: 1, viewer_id: 2)
+artwork_share2 = ArtworkShare.new(artwork_id: 2, viewer_id: 1)

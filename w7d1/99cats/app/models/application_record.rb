69911edd70637f4697cc28fc889f76f0af227047
bcdguz <bcdguz96@gmail.com>
2021-01-04 16:23:33 -0800

    Finishing application_controller

--- w7d1/99cats/app/models/application_record.rb
+++ w7d1/99cats/app/models/application_record.rb
@@ -1,11 +1,4 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  def current_user
-    @current_user ||= User.find_by(session_token: session[:session_token])
-  end
-
-  def login(user)
-    session[:session_token] = user.reset_session_token!
-  end
 end

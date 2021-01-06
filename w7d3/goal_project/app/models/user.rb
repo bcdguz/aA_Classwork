ac572fed1190eaddd2ad88e3cd7682879da9de2e
rupesh5ng <rupesh_singh87@yahoo.com>
2021-01-06 16:29:41 -0600

    finished user controller

--- w7d3/goal_project/app/models/user.rb
+++ w7d3/goal_project/app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     validates :password, length: { minimum: 6, allow_nil: true }
     validates :password_digest, presence: true
     validates :session_token, presence: true, uniqueness: true
-
+    after_initialization :ensure_session_token
     attr_reader :password
 
     #SPIRE
@@ -25,5 +25,15 @@ class User < ApplicationRecord
 
     def is_password?(password)
         BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom::urlsafe_base64
     end
 end

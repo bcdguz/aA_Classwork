3c701f6982724a477bdbd4436b5d8af83810b2ac
bcdguz <bcdguz96@gmail.com>
2021-01-12 16:04:04 -0800

    Working on router.rb

--- w8d2/rails_lite/lib/controller_base.rb
+++ w8d2/rails_lite/lib/controller_base.rb
@@ -8,6 +8,8 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
   end
 
   # Helper method to alias @already_built_response
@@ -16,17 +18,36 @@ class ControllerBase
 
   # Set the response status code and header
   def redirect_to(url)
+    @res.status = '302'
+    @res['Location'] = url
+    @already_built_response = true
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    @res.write(content)
+    @res['content-type'] = content_type
+    raise "double render error" if already_built_response?
+    @already_built_response = true
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    dir_path = File.dirname(__FILE__)
+    template_fname = File.join(
+      dir_path, "..",
+      "views", self.class.name.underscore, "#{template_name}.html.erb"
+    )
+
+    template_code = File.read(template_fname)
+
+    render_content(
+      ERB.new(template_code).result(binding),
+      "text/html"
+    )
   end
 
   # method exposing a `Session` object
--- w8d2/rails_lite/lib/router.rb
+++ w8d2/rails_lite/lib/router.rb
@@ -2,15 +2,25 @@ class Route
   attr_reader :pattern, :http_method, :controller_class, :action_name
 
   def initialize(pattern, http_method, controller_class, action_name)
+    @pattern, @http_method = pattern, http_method
+    @controller_class, @action_name = controller_class, action_name
   end
 
   # checks if pattern matches path and method matches request method
   def matches?(req)
+    (http_method == req.request_method.downcase.to_sym) && !!(pattern =~ req.path)
   end
 
   # use pattern to pull out route params (save for later?)
   # instantiate controller and call controller action
   def run(req, res)
+    match_data = @pattern.match(req.path)
+
+    route_params = Hash[match_data.names.zip(match_data.captures)]
+
+    @controller_class
+      .new(req, res, route_params)
+      .invoke_action(action_name)
   end
 end
 

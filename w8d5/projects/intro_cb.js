08a60708fc29aa73658d0f87ac8e846f0d987805
Janette <tinoconjanette@gmail.com>
2021-01-15 14:11:25 -0800

    work on addNumbers on projects

--- w8d5/projects/intro_cb.js
+++ w8d5/projects/intro_cb.js
@@ -5,7 +5,7 @@ class Clock {
         this.minutes = date.getMinutes();
         this.seconds = date.getSeconds();
         this.printTime();
-        setInterval(this._tick, 1000);
+        setInterval(this._tick.bind(this), 1000);
     }
 
     printTime() {
@@ -13,9 +13,35 @@ class Clock {
     }
 
     _tick() {
-        this.seconds += 1;
+        if (this.seconds === 59){
+            this.seconds = 0;
+            this.minutes += 1;
+        }else{
+            this.seconds += 1;
+        };
+        if (this.minutes === 59) {
+            this.minutes = 0;
+            this.hours += 1;
+        };
+        
         this.printTime();
     }
 }
 
-const clock = new Clock();
+// const clock = new Clock();
+
+const readline = require("readline");
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+function addNumbers(sum, numsLeft, cb){
+    if (numsLeft === 0){
+        cb(sum)
+    }
+    reader.question("Input a number to add", (num) => {
+        sum += parseInt(num)
+        console.log(`Partial sum:${sum}`)
+    })
+}

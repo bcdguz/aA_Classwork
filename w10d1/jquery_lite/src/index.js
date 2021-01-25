8ab255101df9211a7b736dae1e9339d9474cfc42
AlexeyS <xengawe@gmail.com>
2021-01-25 11:45:36 -0800

    complete $l

--- w10d1/jquery_lite/src/index.js
+++ w10d1/jquery_lite/src/index.js
@@ -1,3 +1,9 @@
-window.$l
-const elementList = document.querySelector("li");
-console.log(elementList);
+function $l(selector){
+  const nodeList = document.querySelectorAll(selector);
+  // let elementArr = Array.from(nodeList);
+  let elementArr = Array.prototype.slice.call(nodeList)
+  return elementArr;
+}
+
+window.$l = $l;
+

60539723bf1b9f3357cba0039981760ad174eaec
bcdguz <bcdguz96@gmail.com>
2021-02-01 13:52:42 -0800

    Starting steps

--- w10d4/todos_project/todo_redux.jsx
+++ w10d4/todos_project/todo_redux.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import configureStore from './frontend/store/store';
 import App from './frontend/components/app';
 import Root from './frontend/components/root';
-
+import { stepsByTodoId } from './frontend/reducers/selectors';
 
 document.addEventListener("DOMContentLoaded", () => {
     // console.log('In entry');
@@ -11,6 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const store = configureStore();
     ReactDOM.render(<Root store={store} />, root)
     window.store = store;
+    window.stepsByTodoId = stepsByTodoId;
 })
 
 import { allTodos } from './frontend/reducers/selectors'

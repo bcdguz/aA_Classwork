71710ee2eb6a7a8fd670bc345e0e0339635cb1f5
bcdguz <bcdguz96@gmail.com>
2021-02-02 14:11:06 -0800

    Working on the parties index and show pages

--- w11d2/jbuilder practice/app/controllers/api/guests_controller.rb
+++ w11d2/jbuilder practice/app/controllers/api/guests_controller.rb
@@ -6,6 +6,7 @@ class Api::GuestsController < ApplicationController
 
   def show
     @guest = Guest.find_by(id: params[:id])
+    @gifts = Gift.where(guest_id: params[:id])
     render :show
   end
 end
--- w11d2/jbuilder practice/app/views/api/guests/show.json.jbuilder
+++ w11d2/jbuilder practice/app/views/api/guests/show.json.jbuilder
@@ -1,1 +1,4 @@
-json.extract! @guest, :name, :age, :favorite_color
+json.partial! 'guest', guest: @guest
+json.gifts @gifts do |gift|
+    json.extract! gift, :title, :description
+end

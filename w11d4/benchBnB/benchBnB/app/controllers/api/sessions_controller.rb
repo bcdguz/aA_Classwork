e23ce8b6c2c725be9c75409b2919311085ebab44
Itai Farhi <itaimf@gmail.com>
2021-02-04 15:11:41 -0800

    change file structure

--- w11d4/benchBnB/benchBnB/app/controllers/api/sessions_controller.rb
+++ w11d4/benchBnB/benchBnB/app/controllers/api/sessions_controller.rb
@@ -1,2 +1,25 @@
 class Api::SessionsController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.find_by_credentials(user_params)
+        if @user
+            login!(@user)
+            redirect_to root_url
+        else  
+            @user = User.new
+            flash.now[:errors] = { base:["Invalid username or password"] }
+            render :new
+        end
+    end
+
+    def destroy
+        if logout!
+            render {}
+        else
+            
+        redirect_to new_session_url
+    end
 end

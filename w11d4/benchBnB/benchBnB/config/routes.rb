454690f9cd0383e73767f5e520fcc40f1abc6e9c
bcdguz <bcdguz96@gmail.com>
2021-02-04 14:59:20 -0800

    Implemented create and destroy methods in the sessions controller

--- w11d4/benchBnB/benchBnB/config/routes.rb
+++ w11d4/benchBnB/benchBnB/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   namespace :api, default: {format: :json} do 
     resources :users, only: [:create]
-    resources :sessions, only: [:create, :destroy]
+    resources :session, only: [:new, :create, :destroy]
   end
 
 end

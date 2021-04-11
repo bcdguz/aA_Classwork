f8a7655ed36ed08616f6971f3c778d050974f7e5
bcdguz <bcdguz96@gmail.com>
2021-04-10 18:50:23 -0700

    Adding in root and app components to pass store to all routes

--- Career Quest/practice projects/bencher/frontend/bench_bnb.jsx
+++ Career Quest/practice projects/bencher/frontend/bench_bnb.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { login, logout, signup } from "./actions/session_actions";
 import configureStore from "./store/store";
+import Root from "./components/root";
+import { login, logout, signup } from "./actions/session_actions";
 
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
     const store = configureStore();
-    ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+    ReactDOM.render(<Root store={store} />, root);
 
     //testing functions;
     window.getState = store.getState;

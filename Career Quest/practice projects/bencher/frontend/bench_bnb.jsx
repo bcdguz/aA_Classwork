9820735e123f0e9dcdff368244a64d853fc2447b
bcdguz <bcdguz96@gmail.com>
2021-04-10 18:31:40 -0700

    Configuring store in entry file

--- Career Quest/practice projects/bencher/frontend/bench_bnb.jsx
+++ Career Quest/practice projects/bencher/frontend/bench_bnb.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { login, logout, signup } from "./util/session_api_util";
+import { login, logout, signup } from "./actions/session_actions";
+import configureStore from "./store/store";
+
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
+    const store = configureStore();
     ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
 
+    //testing functions;
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
     window.login = login;
     window.logout = logout;
     window.signup = signup;

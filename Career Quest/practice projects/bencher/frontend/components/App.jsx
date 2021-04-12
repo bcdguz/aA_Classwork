f2a97c2beadc8b4df9985c5e346e3b93f279d6b3
bcdguz <bcdguz96@gmail.com>
2021-04-12 16:56:03 -0700

    Writing handle change React hooks for login form

--- Career Quest/practice projects/bencher/frontend/components/App.jsx
+++ Career Quest/practice projects/bencher/frontend/components/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import GreetingContainer from './greeting/greeting_container';
+import LoginContainer from './session/login_container';
+import {Route} from 'react-router-dom';
 
 const App = () => {
     return (
@@ -8,6 +10,8 @@ const App = () => {
                 <h1>Bench Bnb</h1>
                 <GreetingContainer />
             </header>
+
+            <Route path="/login" component={LoginContainer}></Route>
         </div>
     )
 }
--- Career Quest/practice projects/bencher/frontend/components/session/login.jsx
+++ Career Quest/practice projects/bencher/frontend/components/session/login.jsx
@@ -1,20 +1,37 @@
 import React from 'react';
+import {useState} from 'react';
 
 const LoginComponent = (props) => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     
-    const handleChange = (e) => {
-        debugger
+    const handleChange = (field) => {
+        return (e) => {
+            if (field === 'username') {
+                setUsername(e.target.value);
+            } else {
+                setPassword(e.target.value);
+            }
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const user = {username, password};
+        props.login(user).then(() => {
+                setUsername('');
+                setPassword('');
+            });
     }
 
     return (
         <div className='session-form'>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input body="username" type="text"
-                onChange={handleChange} value={username}/>
+                onChange={handleChange('username')} value={username}/>
                 <input body="password" type="password"
-                onChange={handleChange} value={password} />
+                onChange={handleChange('password')} value={password} />
+                <button>Login</button>
             </form>
         </div>
     )

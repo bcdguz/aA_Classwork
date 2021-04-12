51b1f5e3a49c6cae0cc154230f326b5dd79a291e
bcdguz <bcdguz96@gmail.com>
2021-04-12 16:20:21 -0700

    Writing greeting react component

--- Career Quest/practice projects/bencher/frontend/components/greeting/greeting.jsx
+++ Career Quest/practice projects/bencher/frontend/components/greeting/greeting.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 const Greeting = (props) => {
+    let content = <div></div>;
+    if (props.currentUser !== null) {
+        content = 
+            <div>
+                <h1>Wellcome {props.currentUser.username}!</h1>
+                <button onClick={props.logout}>Logout</button>
+            </div>
+    } else {
+        content =
+            <div>
+                <Link to="/login">Login</Link>
+                <Link to="/signup">Signup</Link>
+            </div>
+    }
+
     return(
-        <div></div>
+        <div className="greeting-header">
+            {content}
+        </div>
     )
 }
 

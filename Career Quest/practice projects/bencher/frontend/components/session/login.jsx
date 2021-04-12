a54e67ca138c6b06f2781c12dc0d1283d5784a66
bcdguz <bcdguz96@gmail.com>
2021-04-12 16:37:23 -0700

    Creating form in login component

--- Career Quest/practice projects/bencher/frontend/components/session/login.jsx
+++ Career Quest/practice projects/bencher/frontend/components/session/login.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 
 const LoginComponent = (props) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    
+    const handleChange = (e) => {
+        debugger
+    }
+
     return (
-        <div></div>
+        <div className='session-form'>
+            <form>
+                <input body="username" type="text"
+                onChange={handleChange} value={username}/>
+                <input body="password" type="password"
+                onChange={handleChange} value={password} />
+            </form>
+        </div>
     )
 }
 

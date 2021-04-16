8d0933dc3c20a5f2a0848645420783006bc85062
bcdguz <bcdguz96@gmail.com>
2021-04-15 18:15:29 -0700

    Writing #index and #show in bench controller

---
+++ Career Quest/practice projects/bencher/app/controllers/api/benches_controller.rb
@@ -0,0 +1,18 @@
+class Api::BenchesController < ApplicationController
+
+    def index
+        @benches = Bench.all
+        render :index
+    end
+
+    def create
+        @bench = Bench.create!(bench_params)
+        render :show
+    end
+
+    private
+
+    def bench_params
+        params.require(:bench).permit(:description, :lat, :lng)
+    end
+end

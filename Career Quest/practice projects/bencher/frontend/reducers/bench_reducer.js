f51a74ac42273bdaea54a14377bc78a42b9c9947
bcdguz <bcdguz96@gmail.com>
2021-04-16 16:46:34 -0700

    Creating benches reducer

--- Career Quest/practice projects/bencher/frontend/reducers/bench_reducer.js
+++ Career Quest/practice projects/bencher/frontend/reducers/bench_reducer.js
@@ -1,3 +1,14 @@
+import { RECEIVE_BENCHES } from "../actions/bench_actions";
+
 const benchReducer = (state = {}, action) => {
-    
+    Object.freeze(state);
+    // const newState = Object.assign({}, state);
+    switch (action.type) {
+        case RECEIVE_BENCHES:
+            return action.benches;
+        default:
+            return state;
+    }
 }
+
+export default benchReducer;
--- Career Quest/practice projects/bencher/frontend/reducers/entities_reducer.js
+++ Career Quest/practice projects/bencher/frontend/reducers/entities_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux"
+import benchReducer from "./bench_reducer";
 import userReducer from "./user_reducer"
 
 const entitiesReducer = combineReducers({
-    users: userReducer
+    users: userReducer,
+    benches: benchReducer
 })
 
 export default entitiesReducer;

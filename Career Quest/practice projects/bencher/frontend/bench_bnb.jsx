e45ea57c015b644a842248ac32025d66a8b82dd5
bcdguz <bcdguz96@gmail.com>
2021-04-20 15:41:15 -0700

    Implementing  markerManager class into bench_map component

--- Career Quest/practice projects/bencher/frontend/bench_bnb.jsx
+++ Career Quest/practice projects/bencher/frontend/bench_bnb.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import configureStore from "./store/store";
 import Root from "./components/root";
+import { fetchBenches } from "./actions/bench_actions";
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
@@ -23,4 +24,5 @@ document.addEventListener("DOMContentLoaded", () => {
     //testing functions;
     window.getState = store.getState;
     window.dispatch = store.dispatch;
+    window.fetchBenches = fetchBenches;
 });
--- Career Quest/practice projects/bencher/frontend/components/benches/bench_map.jsx
+++ Career Quest/practice projects/bencher/frontend/components/benches/bench_map.jsx
@@ -1,16 +1,23 @@
 import React, { useEffect } from 'react';
+import MarkerManager from '../../util/marker_manager';
 
 const BenchMap = (props) => {
     let mapNode;
-
+    let markerManager;
     useEffect(() => {
         const mapOptions = {
             center: {lat: 37.7758, lng: -122.435}, //coords for sf
             zoom: 13
         };
 
-        const myMap = new google.maps.Map(mapNode, mapOptions)
-    },[])
+        const myMap = new google.maps.Map(mapNode, mapOptions);
+        markerManager = new MarkerManager(myMap);
+    }, [])
+
+    useEffect(() => {
+        if (!markerManager) return;
+        markerManager.updateMarkers(props.benches)
+    }, [props.benches])
 
     return (
         <div ref={map => mapNode = map} id='map_container'></div>

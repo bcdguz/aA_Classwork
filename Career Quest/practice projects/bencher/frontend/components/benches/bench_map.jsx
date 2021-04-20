7bb6e7db9d131305410b3b5fb67b124a3f3235ef
bcdguz <bcdguz96@gmail.com>
2021-04-19 17:28:39 -0700

    Getting google maps to render in benchMap component

--- Career Quest/practice projects/bencher/frontend/components/benches/bench_map.jsx
+++ Career Quest/practice projects/bencher/frontend/components/benches/bench_map.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const BenchMap = (props) => {
+    let mapNode;
+
+    useEffect(() => {
+        const mapOptions = {
+            center: {lat: 37.7758, lng: -122.435}, //coords for sf
+            zoom: 13
+        };
+
+        const myMap = new google.maps.Map(mapNode, mapOptions)
+    },[])
+
     return (
-        <div id='map_container'></div>
+        <div ref={map => mapNode = map} id='map_container'></div>
     )
 }
 

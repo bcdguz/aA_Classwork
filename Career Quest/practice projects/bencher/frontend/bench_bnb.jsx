50e7e9c0ae9af19d9b6a1b27a3478d10a27c2bcc
bcdguz <bcdguz96@gmail.com>
2021-04-22 16:10:19 -0700

    Refactoring bench_api_util to include bounds

--- Career Quest/practice projects/bencher/frontend/bench_bnb.jsx
+++ Career Quest/practice projects/bencher/frontend/bench_bnb.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import configureStore from "./store/store";
 import Root from "./components/root";
-import { fetchBenches } from "./actions/bench_actions";
+import {fetchAllBenches} from './util/bench_util';
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
@@ -24,5 +24,5 @@ document.addEventListener("DOMContentLoaded", () => {
     //testing functions;
     window.getState = store.getState;
     window.dispatch = store.dispatch;
-    window.fetchBenches = fetchBenches;
+    window.fetchAllBenches = fetchAllBenches;
 });
--- Career Quest/practice projects/bencher/frontend/util/bench_util.jsx
+++ Career Quest/practice projects/bencher/frontend/util/bench_util.jsx
@@ -1,7 +1,8 @@
-export const fetchAllBenches = () => {
+export const fetchAllBenches = (bounds) => {
     return $.ajax({
         method: "GET",
         url: "/api/benches",
+        data: {bounds},
         error: (error => console.log(error))
     })
 }

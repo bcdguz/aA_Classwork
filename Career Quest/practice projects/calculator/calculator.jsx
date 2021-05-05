b23ad7761f7b6485f03559d91e7f0b84d1a6f9d1
bcdguz <bcdguz96@gmail.com>
2021-05-05 16:56:11 -0700

    Creating buttons and operation handler

--- Career Quest/practice projects/calculator/calculator.jsx
+++ Career Quest/practice projects/calculator/calculator.jsx
@@ -6,6 +6,8 @@ class Calculator extends React.Component{
     this.state = {num1: "", num2: "", result: 0};
     this.setNum1 = this.setNum1.bind(this);
     this.setNum2 = this.setNum2.bind(this);
+    this.clearFields = this.clearFields.bind(this);
+    this.handleOperation= this.handleOperation.bind(this);
   }
 
   setNum1(e){
@@ -23,13 +25,37 @@ class Calculator extends React.Component{
     this.setState({num1: "", num2: ""});
   }
 
+  handleOperation(e){
+    e.preventDefault;
+    const operation = e.currentTarget.innerText;
+    const num1 = this.state.num1;
+    const num2 = this.state.num2;
+    let result;
+    if (operation === "+") {
+      result = num1 + num2;
+    } else if (operation === "-") {
+      result = num1 - num2;
+    } else if (operation === "/") {
+      result = num1 / num2;
+    } else {
+      result = num1 * num2;
+    }
+    this.setState({result})
+  }
+
   render(){
     return (
       <div>
         <h1>{this.state.result}</h1>
         <input onChange={this.setNum1} type="text" value={this.state.num1}/>
         <input onChange={this.setNum2} type="text" value={this.state.num2} />
-        <button>Clear</button>
+        <div className="operations">
+          <button onClick={this.handleOperation}>+</button>
+          <button onClick={this.handleOperation}>-</button>
+          <button onClick={this.handleOperation}>/</button>
+          <button onClick={this.handleOperation}>*</button>
+        </div>
+        <button onClick={this.clearFields}>Clear</button>
       </div>
     );
   }

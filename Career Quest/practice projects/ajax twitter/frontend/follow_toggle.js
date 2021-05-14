54e89a74ea387e9f95567d9fd6a286778f5f7011
bcdguz <bcdguz96@gmail.com>
2021-05-14 16:28:03 -0700

    Creating a click handler for following a user

--- Career Quest/practice projects/ajax twitter/frontend/follow_toggle.js
+++ Career Quest/practice projects/ajax twitter/frontend/follow_toggle.js
@@ -1,7 +1,46 @@
 class FollowToggle {
     constructor(el) {
+        this.button = el;
         this.userId = el.dataset.userId;
         this.followState = el.dataset.initialFollowState;
+        this.render = this.render.bind(this);
+        this.render();
+        this.button.addEventListener('click', (e) => this.handleClick(e));
+    }
+
+    handleClick(e) {
+        e.preventDefault();
+        const button = this;
+        if (this.followState === "unfollowed") {
+            $.ajax({
+                method: "POST",
+                url: `/users/${this.userId}/follow`,
+                datatype: 'json'
+            }).then(res => {
+                button.followState = "followed";
+                console.log("followed")
+                button.render();
+            })
+        } else {
+            $.ajax({
+                method: "DELETE",
+                url: `/users/${this.userId}/follow`,
+                datatype: 'json'
+            }).then(res => {
+                button.followState = "followed";
+                console.log("unfollowed")
+                button.render();
+            })
+        }
+    }
+
+    render() {
+        
+        if (this.followState === "unfollowed") {
+            this.button.innerText = "Follow!";
+        } else {
+            this.button.innerText = "Unfollow!"
+        }
     }
 }
 

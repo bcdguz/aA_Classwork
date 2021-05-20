8d4f35291799c95b510d52debe5094aeb53bc325
bcdguz <bcdguz96@gmail.com>
2021-05-20 16:23:40 -0700

    Creating bench form component

--- Career Quest/practice projects/ajax twitter/frontend/user_search.js
+++ Career Quest/practice projects/ajax twitter/frontend/user_search.js
@@ -2,6 +2,27 @@ class UserSearch {
     constructor(el) {
         this.input = el.children[0];
         this.ul = el.children[1];
+        this.handleInput = this.handleInput.bind(this);
+        this.input.addEventListener('change', (e) => this.handleInput(e));
+    }
+
+    searchUsers(queryVal) {
+        const url = new URL('/users/search/');
+        const params = {query: queryVal}; 
+        url.search = new URLSearchParams(params).toString();
+        fetch(url)
+            .then(res => {
+                return res.json();
+            })
+            .then(res => {
+                debugger
+            })
+    }
+
+    handleInput(e) {
+        const value = e.target.value;
+        this.searchUsers(value);
+        
     }
 }
 

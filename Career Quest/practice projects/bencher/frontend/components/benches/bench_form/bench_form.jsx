13ae16830d0a3955761a3ebf05d17625ab5468bc
bcdguz <bcdguz96@gmail.com>
2021-05-20 16:31:59 -0700

    Adding seating column to benches table

--- Career Quest/practice projects/bencher/frontend/components/benches/bench_form/bench_form.jsx
+++ Career Quest/practice projects/bencher/frontend/components/benches/bench_form/bench_form.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 const BenchForm = (props) => {
 
     return (
-        <div></div>
+        <form>
+            <label>Description<input type="text" /></label>
+            <label>Number of Seats<input type="text" /></label>
+            <label>Latitude<input type="text" /></label>
+            <label>Longitude<input type="text" /></label>
+        </form>
     )
 }
 

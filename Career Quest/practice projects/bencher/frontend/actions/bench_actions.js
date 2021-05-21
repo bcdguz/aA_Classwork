ffd0839987a3e06ce5144d4e2033c0a3e4b50a9b
bcdguz <bcdguz96@gmail.com>
2021-05-21 16:46:47 -0700

    Writing new bench action creator

--- Career Quest/practice projects/bencher/frontend/actions/bench_actions.js
+++ Career Quest/practice projects/bencher/frontend/actions/bench_actions.js
@@ -1,6 +1,14 @@
-import { fetchAllBenches } from "../util/bench_util";
+import { createBench, fetchAllBenches } from "../util/bench_util";
 
 export const RECEIVE_BENCHES = "RECEIVE_BENCHES";
+export const RECEIVE_BENCH = "RECEIVE_BENCH";
+
+const recieveBench = (bench) => {
+    return {
+        type: RECEIVE_BENCH,
+        bench
+    }
+}
 
 const receiveBenches = (benches) => {
     return {
@@ -9,7 +17,13 @@ const receiveBenches = (benches) => {
     }
 } 
 
-export const fetchBenches = (filters) => dispatch => {
+export const newBench = (bench) => (dispatch) => {
+    return createBench(bench).then(
+        (bench) => dispatch(recieveBench(bench))
+    )
+}
+
+export const fetchBenches = (filters) => (dispatch) => {
     return fetchAllBenches(filters)
         .then(benches => dispatch(receiveBenches(benches)));
 }

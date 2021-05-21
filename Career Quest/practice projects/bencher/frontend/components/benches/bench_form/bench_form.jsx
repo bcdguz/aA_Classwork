6ff568da27831ac9fed7c4717261715d4d160b42
bcdguz <bcdguz96@gmail.com>
2021-05-20 17:01:41 -0700

    Passing in lat and lng params to bench form

--- Career Quest/practice projects/bencher/frontend/components/benches/bench_form/bench_form.jsx
+++ Career Quest/practice projects/bencher/frontend/components/benches/bench_form/bench_form.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 
-const BenchForm = (props) => {
+const BenchForm = ({lat, lng}) => {
 
     return (
         <form>
             <label>Description<input type="text" /></label>
             <label>Number of Seats<input type="text" /></label>
-            <label>Latitude<input type="text" /></label>
-            <label>Longitude<input type="text" /></label>
+            <label>Latitude
+                <input disabled={true} type="text" value={lat} />
+            </label>
+            <label>Longitude
+                <input disabled={true} type="text" value={lng}/>
+            </label>
         </form>
     )
 }
--- Career Quest/practice projects/bencher/frontend/components/benches/bench_form/bench_form_container.js
+++ Career Quest/practice projects/bencher/frontend/components/benches/bench_form/bench_form_container.js
@@ -1,5 +1,12 @@
 import { connect } from "react-redux";
 import BenchForm from "./bench_form";
 
-const BenchFormContainer = connect(null, null)(BenchForm);
+const mSTP = (state, {location}) => {
+    return {
+        lat: new URLSearchParams(location.search).get('lat'),
+        lng: new URLSearchParams(location.search).get('lng')
+    }
+}
+
+const BenchFormContainer = connect(mSTP, null)(BenchForm);
 export default BenchFormContainer;

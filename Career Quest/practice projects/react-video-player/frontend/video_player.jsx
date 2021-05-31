462255c055927c33c08fdf574189454934863e51
bcdguz <bcdguz96@gmail.com>
2021-05-31 16:53:24 -0700

    Adding react component

--- Career Quest/practice projects/react-video-player/frontend/video_player.jsx
+++ Career Quest/practice projects/react-video-player/frontend/video_player.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
 class VideoPlayer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.player = document.getElementById('player');
+    }
+
     render() {
+        debugger
         return (
             <div>Hello World</div>
         )

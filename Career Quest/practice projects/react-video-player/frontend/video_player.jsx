99cee39bb8ae0358d92d542b4ad67c98a428c69e
bcdguz <bcdguz96@gmail.com>
2021-05-31 17:00:11 -0700

    Implementing iframe via react component

--- Career Quest/practice projects/react-video-player/frontend/video_player.jsx
+++ Career Quest/practice projects/react-video-player/frontend/video_player.jsx
@@ -3,13 +3,23 @@ import React from 'react'
 class VideoPlayer extends React.Component {
     constructor(props) {
         super(props);
-        this.player = document.getElementById('player');
+
     }
 
     render() {
-        debugger
+        const embedId = "Dx5DOi_8-uc"
         return (
-            <div>Hello World</div>
+            <div>
+                <iframe
+                    width="720"
+                    height="480"
+                    src={`https://www.youtube.com/embed/${embedId}`}
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                    title="Embedded youtube"
+                />
+            </div>
         )
     }
 }

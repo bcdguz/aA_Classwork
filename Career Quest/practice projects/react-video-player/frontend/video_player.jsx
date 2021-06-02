01809621369659e52bed9bee2b3b66e0b55025f2
bcdguz <bcdguz96@gmail.com>
2021-06-02 16:49:56 -0700

    Removing default controls from yt video

--- Career Quest/practice projects/react-video-player/frontend/video_player.jsx
+++ Career Quest/practice projects/react-video-player/frontend/video_player.jsx
@@ -9,16 +9,17 @@ class VideoPlayer extends React.Component {
     render() {
         const embedId = "Dx5DOi_8-uc"
         return (
-            <div class="video-container">
+            <div className="video-container">
                 <iframe
                     width="720"
                     height="480"
-                    src={`https://www.youtube.com/embed/${embedId}`}
+                    src={`https://www.youtube.com/embed/${embedId}?controls=0`}
                     frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen
                     title="Embedded youtube"
                 />
+                <div className="controls-container"></div>
             </div>
         )
     }

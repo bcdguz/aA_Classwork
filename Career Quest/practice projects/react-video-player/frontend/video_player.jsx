234348c02e579955bd34a708fc69e99199b7f4f8
bcdguz <bcdguz96@gmail.com>
2021-06-02 17:09:53 -0700

    Inserting play line

--- Career Quest/practice projects/react-video-player/frontend/video_player.jsx
+++ Career Quest/practice projects/react-video-player/frontend/video_player.jsx
@@ -24,6 +24,9 @@ class VideoPlayer extends React.Component {
                     <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-play-circle-fill" viewBox="0 0 16 16">
                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z" />
                     </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="bi bi-dash-lg" viewBox="0 0 16 16">
+                        <path d="M0 8a1 1 0 0 1 1-1h14a1 1 0 1 1 0 2H1a1 1 0 0 1-1-1z" />
+                    </svg>
                 </div>
             </div>
         )

adcec9cd26b8acd7d38e7a01b3936133d0b82f99
bcdguz <bcdguz96@gmail.com>
2021-06-10 18:51:40 -0700

    Writing an onchange for video time input

--- Career Quest/practice projects/react-video-player/frontend/video_player.jsx
+++ Career Quest/practice projects/react-video-player/frontend/video_player.jsx
@@ -4,7 +4,11 @@ import React from 'react';
 class VideoPlayer extends React.Component {
     constructor(props) {
         super(props);
+    }
 
+    changeTime = (e) => {
+        e.preventDefault();
+        const 
     }
 
     render() {
@@ -24,7 +28,7 @@ class VideoPlayer extends React.Component {
                     <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-play-circle-fill" viewBox="0 0 16 16">
                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z" />
                     </svg>
-                    <input type="range" name="playbackRate" class="player__slider"
+                    <input type="range" name="playbackRate" className="player__slider"
                     min="0.5" max="2" step="0.1" value="1" />
                 </div>
             </div>

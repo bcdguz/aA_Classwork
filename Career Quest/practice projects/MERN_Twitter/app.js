9bb994d04553c6b92172d3061e964e7ccf89e9a5
bcdguz <bcdguz96@gmail.com>
2021-06-14 17:15:55 -0700

    Adding body parser to parse JSON

--- Career Quest/practice projects/MERN_Twitter/app.js
+++ Career Quest/practice projects/MERN_Twitter/app.js
@@ -3,13 +3,23 @@ const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
+const bodyParser = require("body-parser");
+//route imports
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB succesfully"))
     .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Working"));
+//body parser to parse json
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+//routes
+app.get("/", (req, res) => res.send("HelloWorld"));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 
